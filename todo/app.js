b87d7cde35fb1b579a2147c0774352cc2e157d40
tkmct <1220t.takamichi@gmail.com>
2019-01-01 02:30:19 +0900

    todo mvc

--- todo/app.js
+++ todo/app.js
@@ -1,1 +1,29 @@
-console.log('hello')
+const todos = [
+  'Read book',
+  'Buy coffee',
+  'Study'
+]
+
+function listItem(item) {
+  const li = document.createElement('li')
+  li.appendChild(document.createTextNode(item))
+  return li
+}
+
+function render() {
+  const listRef = document.getElementById('todo-list')
+  listRef.innerHtml = ''
+
+  todos.forEach(todo => {
+    listRef.appendChild(listItem(todo))
+  })
+}
+
+function addItem() {
+  const listRef = document.getElementById('todo-list')
+  const item = document.getElementById('item-input').value
+  todos.push(item)
+  listRef.appendChild(listItem(item))
+}
+
+render()
