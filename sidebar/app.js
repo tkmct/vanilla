311f1a75d0c346c33cb7286a30a967db628f04f3
tkmct <1220t.takamichi@gmail.com>
2019-01-02 03:02:24 +0900

    sidebar

--- sidebar/app.js
+++ sidebar/app.js
@@ -1,2 +1,12 @@
 'use strict';
 
+let isOpen = false;
+
+function toggle() {
+  if (!isOpen) {
+    document.getElementById('side-nav').classList.add('open');
+  } else {
+    document.getElementById('side-nav').classList.remove('open');
+  }
+  isOpen = !isOpen;
+}
---
+++ sidebar/js/index.js
@@ -0,0 +1,10 @@
+let isOpen = false
+
+function toggle() {
+  if (!isOpen) {
+    document.getElementById('side-nav').classList.add('open')
+  } else {
+    document.getElementById('side-nav').classList.remove('open')
+  }
+  isOpen = !isOpen
+}
