037b891072f9a355a0d8a5afc432f241f35c8a73
tkmct <1220t.takamichi@gmail.com>
2019-01-04 03:46:43 +0900

    shift table initial

---
+++ shifttable/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const shiftData = [
+  {name: 'Takamichi', shifts: [ {start: '12:00', end: '16:00'} ]},
+  {name: 'Bob', shifts: [ {start: '15:00', end: '20:00'} ]},
+  {name: 'Doe', shifts: [ {start: '16:00', end: '22:00'} ]},
+  {name: 'Jane', shifts: [ {start: '18:00', end: '22:00'} ]},
+  {name: 'Tim', shifts: [ {start: '18:00', end: '22:00'} ]},
+  {name: 'Tommy', shifts: [ {start: '20:00', end: '22:00'} ]},
+];
+
+const data = {
+  shiftStartTime: '12:00',
+  shifts: shiftData
+};
