477bc5c76c474b80984a8acc7a872c1bd2a9010f
tkmct <1220t.takamichi@gmail.com>
2019-01-05 18:26:39 +0900

    Show and hide tooltip

--- tooltip/app.js
+++ tooltip/app.js
@@ -4,12 +4,24 @@ const button = document.getElementById('tooltip-button');
 const root = document.getElementById('tooltip-root');
 
 button.style.position = 'relative';
-button.addEventListener('click', () => {
-  toggleTooltip(root, button);
+button.addEventListener('click', (e) => {
+  e.stopPropagation();
+  showTooltip(root, button);
 });
 
-function toggleTooltip(root, button) {
+window.addEventListener('click', (e) => {
+  hideTooltip(root);
+});
+
+function showTooltip(root, button) {
   const content = document.createElement('div');
   content.appendChild(document.createTextNode('Hello, world'));
   root.appendChild(content);
+}
+
+function hideTooltip(root) {
+  const content = root.firstChild;
+  if (content) {
+    root.removeChild(content);
+  }
 }
